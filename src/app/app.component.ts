ca79bf81a2e3a63b5707be104c65692bd1f00f97
rajesh <rajeshsudam2001@gmail.com>
2022-05-19 18:43:18 +0530

    updated version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  response;
+  constructor(private http:HttpClient ) {}
+  ngOnInit() {          
+    this.http.get<any>('https://cat-fact.herokuapp.com/facts').subscribe(data => {
+        this.response = data;
+    })
+}
 }
